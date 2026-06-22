3f59907e46f659e84de9382a8e34ab52051f1d8b
echaustre12 <124590551+echaustre12@users.noreply.github.com>
2026-06-22 17:59:04 -0500

    fix: Solve maintainability problems detected by SonarQube (#7)
    * feat: include postgresql and spring boot dependencies
    
    * feat: include the entity classes using JPA, include SonarQube workflow
    
    * fix: refactor on remote branch
    
    * fix: Solve maintainability problems detected by SonarQube

--- src/main/java/com/financetrack/model/CategoryBudgetId.java
+++ src/main/java/com/financetrack/model/CategoryBudgetId.java
@@ -1,12 +1,35 @@
 package com.financetrack.model;
 
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.Embedded;
 
 @Embeddable
 public class CategoryBudgetId {
+
     private long categoryId;
     private long financialPeriodId;
 
-    public CategoryBudgetId() {}
+    protected CategoryBudgetId() {
+        // Constructor requerido por JPA
+    }
+
+    public CategoryBudgetId(long categoryId, long financialPeriodId) {
+        this.categoryId = categoryId;
+        this.financialPeriodId = financialPeriodId;
+    }
+
+    public long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public long getFinancialPeriodId() {
+        return financialPeriodId;
+    }
+
+    public void setFinancialPeriodId(long financialPeriodId) {
+        this.financialPeriodId = financialPeriodId;
+    }
 }
