02bd78d22331d85664802d0a930bd0f973c90729
echaustre12 <124590551+echaustre12@users.noreply.github.com>
2026-07-08 23:27:07 -0500

    feat: User service and repository created, SecurityConfig created (#10)
    * feat: include postgresql and spring boot dependencies
    
    * feat: include the entity classes using JPA, include SonarQube workflow
    
    * fix: refactor on remote branch
    
    * fix: Solve maintainability problems detected by SonarQube
    
    * feat: Add savings entities, some minor changes in other entities
    
    * fix: delete prueba.html
    
    * feat: frontend implemented using React
    
    * feat: User service and repository created, SecurityConfig created

---
+++ src/main/java/com/financetrack/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.financetrack.config;
+
+import com.financetrack.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+                )
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/auth/**"
+                        )
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+
+                .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserService userService, PasswordEncoder passwordEncoder) {
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setUserDetailsService(userService);
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
